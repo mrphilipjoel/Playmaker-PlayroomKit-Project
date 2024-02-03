ee1f5f0fe31c4816374882d7b563e7b86693b438
mrphilipjoel <39815836+mrphilipjoel@users.noreply.github.com>
2024-02-03 12:47:05 -0500

    InsertCoin needed to be in Awake and not Start for some reason.

--- Assets/PlayroomKit Playmaker Integration/PlayroomKitManager.cs
+++ Assets/PlayroomKit Playmaker Integration/PlayroomKitManager.cs
@@ -1,18 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayroomKitManager : MonoBehaviour
+namespace GooglyEyesGames.PlaymakerIntegrations.PlayroomKit
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    using UnityEngine;
+    using Playroom;
 
-    // Update is called once per frame
-    void Update()
+    public class PlayroomKitManager : MonoBehaviour
     {
-        
+        public static PlayroomKitManager Instance { get; private set; }
+
+        [Header("PlayroomKit Options")]
+        public bool streamMode = false;
+        public bool allowGamepads = false;
+        public int maxPlayersPerRoom = 4;
+        public bool skipLobby = false;
+        public int reconnectGracePeriodInSeconds = 30;
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+
+#if UNITY_EDITOR
+            streamMode = true;
+            int randomInt = Random.Range(1111, 9999);
+#endif
+            PlayroomKit.InitOptions options = new PlayroomKit.InitOptions
+            {
+#if UNITY_EDITOR
+                roomCode = randomInt.ToString(),
+#endif
+                streamMode = streamMode,
+                allowGamepads = allowGamepads,
+                maxPlayersPerRoom = maxPlayersPerRoom,
+                skipLobby = skipLobby,
+                reconnectGracePeriod = reconnectGracePeriodInSeconds * 1000,
+            };
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+WebGLInput.captureAllKeyboardInput = false;
+
+#endif
+            PlayroomKit.InsertCoin(() =>
+            {
+                PlayMakerFSM.BroadcastEvent("PlayroomKit/InsertCoin");
+
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            WebGLInput.captureAllKeyboardInput = true;
+#endif
+            });
+/*            void Start()
+            {
+                PlayroomKit.InsertCoin(() =>
+                {
+                    PlayMakerFSM.BroadcastEvent("PlayroomKit/InsertCoin");
+
+
+#if !UNITY_EDITOR && UNITY_WEBGL
+            WebGLInput.captureAllKeyboardInput = true;
+#endif
+                });
+            }*/
+
+        }
     }
 }
+
