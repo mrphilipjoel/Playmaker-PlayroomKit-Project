bb9bac28487692d277f4ac7184f901b38d20a94c
mrphilipjoel <39815836+mrphilipjoel@users.noreply.github.com>
2024-02-10 15:47:19 -0500

    Fixed issue where I couldn't apply fsmvar

--- Assets/PlayroomKit Playmaker Integration/PlayroomKitActions/PlayroomKitGetGameState.cs
+++ Assets/PlayroomKit Playmaker Integration/PlayroomKitActions/PlayroomKitGetGameState.cs
@@ -6,6 +6,7 @@ namespace GooglyEyesGames.PlaymakerIntegrations.PlayroomKit.Actions
     using HutongGames.PlayMaker.Actions;
     using static HutongGames.PlayMaker.FsmEventTarget;
     using Unity.VisualScripting;
+    using HutongGames.Utility;
 
     [ActionCategory("PlayroomKit")]
 	public class PlayroomKitGetGameState : FsmStateAction
@@ -20,36 +21,35 @@ namespace GooglyEyesGames.PlaymakerIntegrations.PlayroomKit.Actions
         public FsmBool reliable;
         public bool everyFrame;
 
+        private Fsm fromFsm;
+
         private void GetTheState()
         {
             switch (variable.Type)
             {
 
                 case VariableType.Float:
-                    variable.floatValue = Playroom.PlayroomKit.GetState<float>(stateKey.Value);
-                    variable.UpdateValue();
+                    FsmFloat _floatTarget = fromFsm.Variables.GetFsmFloat(variable.variableName);
+                    _floatTarget.Value = Playroom.PlayroomKit.GetState<float>(stateKey.Value);
                     break;
                 case VariableType.Int:
-                    variable.intValue = Playroom.PlayroomKit.GetState<int>(stateKey.Value);
-                    variable.UpdateValue();
+                    FsmInt _intTarget = fromFsm.Variables.GetFsmInt(variable.variableName);
+                    _intTarget.Value = Playroom.PlayroomKit.GetState<int>(stateKey.Value);
                     break;
                 case VariableType.Bool:
-                    variable.boolValue = Playroom.PlayroomKit.GetState<bool>(stateKey.Value);
-                    variable.UpdateValue();
+                    FsmBool _boolTarget = fromFsm.Variables.GetFsmBool(variable.variableName);
+                    _boolTarget.Value = Playroom.PlayroomKit.GetState<bool>(stateKey.Value);
                     break;
                 case VariableType.String:
-                    variable.stringValue = Playroom.PlayroomKit.GetState<string>(stateKey.Value);
-                    variable.UpdateValue();
-                    Debug.LogWarning(variable.stringValue);
+                    FsmString _stringTarget = fromFsm.Variables.GetFsmString(variable.variableName);
+                    _stringTarget.Value = Playroom.PlayroomKit.GetState<string>(stateKey.Value);
                     break;
                 default:
                     Debug.LogError("Variable type MUST be float, int, bool, or string!");
                     break;
             }
 
-
-
-
+            Debug.Log("Attempted to UpdateValue of variable: " + variable);
         }
 
         public override void OnUpdate()
@@ -59,7 +59,7 @@ namespace GooglyEyesGames.PlaymakerIntegrations.PlayroomKit.Actions
 
         public override void OnEnter()
         {
-
+            fromFsm = Fsm;
             GetTheState();
             if (!everyFrame)
             {
